f6da2b373ce70ef932df0902a2a5b46afbe07aa4
João Patrício <joao.patricio@kuink.org>
2018-05-17 09:07:53 +0100

    Add plain Strategy

--- lib/adapter/authentication/PlainStrategy.php
+++ lib/adapter/authentication/PlainStrategy.php
@@ -1,13 +1,35 @@
-<?php 
+<?php
+
 namespace Kuink\Adapter\Authentication;
 
 class PlainStrategy extends UsernamePasswordStrategy
 {
     const PASSWORD_HASH_COST = 12;
 
-    public function authenticate() : bool
+    public function authenticate(): bool
     {
+
+        $fwUser = $this->getFrameworkUser($this->username);
+
+
+
         return $this->username == 'admin' && $this->comparePasswordWithHash('$2y$12$hzSVsFCC37UzAJ8fj5IwOuFHzno7k7KDSdAutxZcK0MnTH/yGn0si', $this->password);
+    }
+
+    protected function getFrameworkUser(string $username)
+    {
+        global $KUINK_BRIDGE_CFG;
+        try {
+            // Load the user companies
+            $datasource = new \Kuink\Core\DataSource (null, 'framework/framework,user,user.getUser', null, null);
+            $pars = array(
+                'username' => $username
+            );
+            $user = $datasource->execute($pars);
+        } catch (\Exception $e){
+            die($e);
+        }
+
     }
 
 
@@ -16,7 +38,8 @@ class PlainStrategy extends UsernamePasswordStrategy
      * @param string $password
      * @return string
      */
-    protected function passwordToHash(string $password) : string{
+    protected function passwordToHash(string $password): string
+    {
         $options = ['cost' => self::PASSWORD_HASH_COST];
         return password_hash($password, PASSWORD_DEFAULT, $options);
     }
@@ -27,7 +50,8 @@ class PlainStrategy extends UsernamePasswordStrategy
      * @param string $plainPassword
      * @return bool
      */
-    protected function comparePasswordWithHash(string $hash, string $plainPassword){
+    protected function comparePasswordWithHash(string $hash, string $plainPassword)
+    {
         return password_verify($plainPassword, $hash);
     }
 }
