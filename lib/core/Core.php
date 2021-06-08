e0629fcb7d147f88e8f0bd1a65f32cc14a0eace4
Paulo Tavares <paulo.tavares@cscm-lx.pt>
2021-06-08 14:34:43 +0100

    Fix: core | php error reporting to hide warnings when set to false

--- lib/core/Core.php
+++ lib/core/Core.php
@@ -53,6 +53,9 @@ class Core {
 		if (isset($KUINK_CFG->displayNativeErrors) && $KUINK_CFG->displayNativeErrors) {
 			error_reporting(E_ALL);
 			ini_set('display_errors', 1);
+		} else {
+			error_reporting(0);
+			ini_set('display_errors', 0);
 		}
 		
 
