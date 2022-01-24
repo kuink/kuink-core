6e8d2f9aa66118f6bd3e46bd085f33bb9b2fe5f9
Paulo Tavares <paulo.tavares@cscm-lx.pt>
2022-01-24 11:24:41 +0000

    Fix: error | Production error

---
+++ lib/core/Person.php
@@ -0,0 +1,15 @@
+<?php
+namespace Kuink\Core;
+
+class PersonProperty {
+	const ID = 'id';
+	const UID = 'uid';
+	const GIVEN_NAME = 'given_name';
+	const SURNAME = 'surname';
+	const EMAIL = 'email';
+	const PASSWORD = 'password';
+
+	const RECOVERY_EMAIL = 'recovery_email';
+	const CHANGE_PASSWORD = 'change_password';
+
+}
