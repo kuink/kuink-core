54addc2d9e67913efba25b60d076289792f24e49
Paulo Tavares <paulo.tavares@cscm-lx.pt>
2022-02-04 11:27:42 +0000

    Fix: message | simple messages are not being delivered

--- lib/lib/MessageLib.php
+++ lib/lib/MessageLib.php
@@ -101,7 +101,8 @@ class MessageLib {
 		$msg->setBody ( $params ['body'] );
 		$msg->setHeaders ( $headers );
 		if ($KUINK_CFG->enableEmailSending) {
-			\Kuink\Core\TraceManager::add ( 'Sending email to: '.$_headers->To, \Kuink\Core\TraceCategory::GENERAL, __CLASS__ );  	
+			\Kuink\Core\TraceManager::add ( 'ZEND | Sending email to: '.$_headers->To, \Kuink\Core\TraceCategory::GENERAL, __CLASS__ );  	
+			\Kuink\Core\TraceManager::add ( 'ZEND | Sending email content '.$params ['body'], \Kuink\Core\TraceCategory::GENERAL, __CLASS__ );  	
 			$transport = new Zend\Mail\Transport\Sendmail ();
 			$transport->send ( $msg );
 		}
