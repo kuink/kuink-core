057bbe037de0a06f24db239442ed3cbf9a1ea4ee
jose.rebelo.kuink <crebelo.jeduardo@hotmail.com>
2022-07-27 14:59:27 +0100

    Feature: jsonDecode: add the possibility to decode into an associative array

--- lib/lib/UtilsLib.php
+++ lib/lib/UtilsLib.php
@@ -195,7 +195,12 @@ substr ( $charid, 0, 8 ) . $hyphen . substr ( $charid, 8, 4 ) . $hyphen . substr
 	}
 	function jsonDecode($params) {
 		$str = $params [0];
-		return json_decode ( $str );
+		$associative = $params [1];
+
+		if ( is_null ( $associative ) || $associative == '' )
+			return json_decode ( $str );
+		else
+			return json_decode ( $str, $associative );
 	}
 	
 	/**
