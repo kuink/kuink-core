cca1c3a7d27a080d2861a9916b70954fe8b8c27a
catarina.fernandes <catarina.fernandes@cscm-lx.pt>
2023-10-23 10:42:50 +0100

    Feature: connector | googleAPIAdminSDKConnector | Improving user handler and implementing group handler to allow groups management.

--- lib/core/PersonGroup.php
+++ lib/core/PersonGroup.php
@@ -4,16 +4,20 @@ namespace Kuink\Core;
 class PersonGroupProperty {
 	const ID = 'id';
 	const UID = 'uid';
+	const CODE = 'code';
+	const NAME = 'name';
 	const DISPLAY_NAME = 'display_name';
 	const DESCRIPTION = 'description';
 	const MAIL_NICK_NAME = 'mail_nickname';
+	const EMAIL = 'email';
 
 	const LOCATION = 'location';
-    const ALLOW_EXTERNAL_SENDERS = 'allow_external_senders';
+  const ALLOW_EXTERNAL_SENDERS = 'allow_external_senders';
 	const AUTO_SUBSCRIBE_NEW_MEMBERS = 'auto_subscribe_new_members';
 
-    const VISIBILITY = 'visibility';
+  const VISIBILITY = 'visibility';
 
+	const TOTAL_MEMBERS = 'total_members';
 	const IS_OWNER = 'is_owner';
 	const IS_MEMBER = 'is_member';
 
